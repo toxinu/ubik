a25b719956528c38f61343044e24b69a5b2130e8
Socketubs <geoffrey@lehee.name>
2012-08-23 16:43:51 +0200

    Add module infos

--- ubik/__init__.py
+++ ubik/__init__.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+__title__ = 'ubik'
+__version__ = '0.1'
+__author__ = 'Geoffrey Lehee'
+__license__ = 'AGPL3'
+__copyright__ = 'Copyright 2012 Geoffrey Lehee'
+
 import core
