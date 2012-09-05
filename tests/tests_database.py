7ee9f799b64ab38d692d756295322340a624c52b
Socketubs <geoffrey@lehee.name>
2012-09-05 20:14:42 +0200

    Add some tests

---
+++ tests/tests_database.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import tests_env
+import unittest
+
+import ubik.core as api
+
+class TestDatabase(unittest.TestCase):
+
+	def setUp(self):
+		self.db = api.db
+
+	def test_01_sync(self):
+		self.db.sync()
+
+	def test_02_misc(self):
+		self.assertEqual(self.db.count_installed(), 0)
+		self.assertEqual(self.db.get_installed(), [])
+
+	def test_03_find_package(self):
+		self.assertTrue(self.db.packages.get('test_deps'))
+		self.assertTrue(self.db.get('test_deps'))
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ tests/tests_env.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+os.environ['UBIK_CONF'] = 'etc/ubik.conf'
