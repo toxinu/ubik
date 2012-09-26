16d01f8150131fa7d402dd483071bca08dae1b3b
toxinu <toxinu@gmail.com>
2012-09-26 10:45:29 +0200

    Add new test_14 test

--- tests/README.rst
+++ tests/README.rst
@@ -1,14 +1,20 @@
-# test_00_create_package
+# Tests
+
+Tests are executed in virtualenv, test_00 create different package, test_01 create local repo and another bunch of tests do the job.
+
+Test machine is forced to be a DEBIAN 6 i386 with ubik configuration file.
+
+## test_00_create_package
 
 - Create as many package as different package resolution I want
 - Test the ubik-package from ubik-toolbelt
 
-# test_01_create_repo
+## test_01_create_repo
 
 - Create repo with previous packages
 - Test the ubik-repo from ubik-toolbelt
 
-# test_10_database
+## test_10_database
 
 - Sync db with repo
 - Check if no package installed
@@ -16,7 +22,7 @@
 - Check simple package query
 - Check package query with regex
 
-# test_11_package_01_02
+## test_11_package_01_02
 
 - Package 01 is available and good system with package_02 as dep
 - Package 02 is available and good system with no dep
@@ -31,12 +37,19 @@
 - Check pacakge content files are removed
 - Some api methods
 
-# test_12_package_03
+## test_12_package_03
 
 - Same as previous but with package_03, which is available, good system and no dep
 
+## test_13_package_04_05
 
-## Packages informations
+- Same as test_11_package_01_02 but packages are (noarch/nodist/novers)
+
+## test_14_package_06
+
+- Try to install noarch/nodist/novers package with itself in deps but must fail
+
+### Packages informations
 
 Package Available:
     Good System:
@@ -47,7 +60,7 @@ Package Available:
     Any System:
       - with one dep(success)      -> package_04    X
       - without dep(success)       -> package_05    X
-      - with itself in dep(fail)   -> package_06
+      - with itself in dep(fail)   -> package_06    X
 
     Other:
       - Bad arch(fail)             -> package_07
---
+++ tests/tests/test_14_package_06.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+
+import env
+import unittest
+
+import ubik.core as api
+
+from ubik.installer import Installer
+from ubik.exceptions import InstallerException
+
+"""
+Package Available:
+    Good System:
+      - iteself in deps(failed)   -> package_06
+"""
+
+class TestPackage_06(unittest.TestCase):
+
+    def setUp(self):
+        self.db = api.db
+        self.package = self.db.packages['package_06']
+
+    def test_01_name(self):
+        self.assertEqual(str(self.package.name), 'package_06')
+
+    def test_02_requires(self):
+        self.assertEqual(self.package.requires, ['package_06'])
+
+    def test_03_arch(self):
+        self.assertEqual(str(self.package.arch), 'noarch')
+
+    def test_04_dist(self):
+        self.assertEqual(str(self.package.dist), 'nodist')
+
+    def test_05_vers(self):
+        self.assertEqual(str(self.package.vers), 'novers')
+
+    def test_06_version(self):
+        self.assertEqual(str(self.package.version), '')
+
+    def test_07_release(self):
+        self.assertEqual(str(self.package.release), '')
+
+    def test_08_repo_version(self):
+        self.assertEqual(str(self.package.repo_version), '0.1')
+
+    def test_09_repo_release(self):
+        self.assertEqual(str(self.package.repo_release), '0')
+
+    def test_100_install_package(self):
+        self.db.sync()
+        installer = Installer()
+	try:
+		installer.resolv(self.package)
+		raise Exception('Dependences resolving must failed')
+	except InstallerException:
+		pass
+        self.assertEqual(len(installer.packages), 0)
+        self.assertTrue(self.db.get('package_06'))
+
+	# Just package_03 installed
+        self.assertEqual(self.db.count_installed(), 1)
+
+	path = "%s/bin/package_06" % api.conf.get('settings', 'packages')
+	self.assertFalse(os.path.exists(path))
+
+if __name__ == '__main__':
+    unittest.main()
