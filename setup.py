32d804cf5cf8f7060378ef6dc58c7c673c5cb4eb
Socketubs <geoffrey@lehee.name>
2012-10-11 09:56:19 +0200

    Add get_version function in setup.py

--- setup.py
+++ setup.py
@@ -3,32 +3,43 @@
 
 import os
 import sys
+import re
 
 try:
 	from setuptools import setup
 except ImportError:
 	from distutils.core import setup
 
+def get_version():
+    VERSIONFILE="ubik/__init__.py"
+    initfile_lines = open(VERSIONFILE, "rt").readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 if sys.argv[-1] == 'publish':
 	os.system('python setup.py sdist upload')
 	sys.exit()
 
 setup(
-	name='ubik',
-	version='0.1.3',
-	description='Minimal packages manager',
-	long_description=	open('README.rst').read() + '\n\n' +
+	name = 'ubik',
+	version = get_version(),
+	description = 'Minimal packages manager',
+	long_description =	open('README.rst').read() + '\n\n' +
 						open('API.rst').read() + '\n\n' +
 						open('HISTORY.rst').read(), 
-	license=open("LICENSE").read(),
-	author="Geoffrey Lehee",
-	author_email="geoffrey@lehee.name",
-	url='https://github.com/socketubs/Ubik/',
-	keywords="ubik package linux",
+	license = open("LICENSE").read(),
+	author = "Geoffrey Lehee",
+	author_email = "geoffrey@lehee.name",
+	url = 'https://github.com/socketubs/Ubik/',
+	keywords = "ubik package linux",
 	packages = ['ubik'],
-	scripts=['scripts/ubik','scripts/echo_ubik_conf'],
-	install_requires=['progressbar==2.3', 'requests', 'docopt==0.5.0', 'isit==0.1.3'],
-	classifiers=(
+	scripts = ['scripts/ubik','scripts/echo_ubik_conf'],
+	install_requires = ['progressbar==2.3', 'requests', 'docopt==0.5.0', 'isit==0.1.3'],
+	classifiers = (
 		'Intended Audience :: Developers',
 		'Natural Language :: English',
 		'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
