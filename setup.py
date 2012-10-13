0df013db8782636aa8d082582f3438e467c59bd3
toxinu <toxinu@gmail.com>
2012-10-13 17:32:10 +0200

    Update version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     keywords = 'ubik package linux',
     packages = ['ubik','ubik_toolbelt'],
     scripts = ['scripts/ubik','scripts/echo_ubik_conf','scripts/ubik-package','scripts/ubik-repo'],
-    install_requires = ['progressbar==2.3', 'requests', 'docopt==0.5.0', 'isit==0.1.3'],
+    install_requires = ['progressbar==2.3', 'requests==0.14.1', 'docopt==0.5.0', 'isit==0.1.3'],
     classifiers = (
         'Intended Audience :: Developers',
         'Natural Language :: English',
--- ubik_toolbelt/__init__.py
+++ ubik_toolbelt/__init__.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 __title__ = 'ubik_toolbelt'
-__version__ = '0.1'
+__version__ = '0.2.0'
 __author__ = 'Geoffrey Lehée'
 __license__ = 'AGPL3'
 __copyright__ = 'Copyright 2012 Geoffrey Lehée'
